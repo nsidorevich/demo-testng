776050bcd78f08778f4a0935263e665511d98a56
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-17 13:33:50 +0300

    Initial commit

---
+++ src/test/java/com/zebrunner/DummyServiceTest.java
@@ -0,0 +1,14 @@
+package com.zebrunner;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class DummyServiceTest {
+
+    @Test()
+    public void firstTest() {
+        assertTrue(Boolean.TRUE);
+    }
+
+}
