9419d4d894b2a2ab8904045cbb51b9cbf855da53
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-17 16:27:51 +0300

    005. Logging - logback

--- src/test/java/com/zebrunner/DummyServiceTest.java
+++ src/test/java/com/zebrunner/DummyServiceTest.java
@@ -1,37 +1,73 @@
 package com.zebrunner;
 
-import com.zebrunner.agent.core.annotation.JiraReference;
-import com.zebrunner.agent.core.annotation.Maintainer;
-import com.zebrunner.agent.core.annotation.Priority;
-import com.zebrunner.agent.core.annotation.TestLabel;
-import com.zebrunner.agent.core.registrar.Label;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
 
-@Maintainer("admin")
-@Priority(Priority.P2)
-@TestLabel(name = "class-level-name", value = "class-level-value")
 public class DummyServiceTest {
 
+    private static final Logger log = LoggerFactory.getLogger(DummyServiceTest.class);
+
+    // not supported!
+    @BeforeClass
+    public void beforeClass() {
+        log.info("BEFORE CLASS");
+    }
+
+    @BeforeMethod
+    public void beforeTest() {
+        log.info("BEFORE METHOD: Test execution is about to start...");
+    }
+
+    // not supported!
+    @AfterMethod
+    public void afterTest() {
+        log.info("AFTER METHOD: Test execution completed");
+    }
+
     @Test(testName = "Test #1")
-    @Priority(Priority.P1)
     public void firstTest() {
+        log.info("Doing things: complex assertions, you know...");
+        waitHooks();
         assertTrue(Boolean.TRUE);
     }
 
     @Test()
-    @Maintainer("anonymous")
-    @TestLabel(name = "stability", value = "poor")
     public void secondTest() {
+        log.info("Info log");
+
+
+        for (int i = 0; i < 10; i++) {
+            log.info("Info log #" + i + " captured at " + System.currentTimeMillis());
+        }
+
+        log.debug("Debug log");
+        log.trace("Trace log");
+        log.warn("Warn log");
+        log.error("Oh no!");
+
+        waitHooks();
+
         assertTrue(Boolean.TRUE);
     }
 
     @Test()
-    @JiraReference("SPG-0001") // or XRay, or TestRail, or QTest or all at once
     public void thirdTest() {
-        Label.attach("new-label", "new-value");
-        assertTrue(Boolean.TRUE);
+        waitHooks();
+        assertTrue(Boolean.FALSE);
+    }
+
+    private void waitHooks() {
+        try {
+            Thread.sleep(1000); // workaround for shutdown hooks issue
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
