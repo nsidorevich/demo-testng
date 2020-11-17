d3186184f7ad352ecc1d7d101d76947a6ad607ac
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-17 15:03:25 +0300

    003. Maintainer and test name override

--- src/test/java/com/zebrunner/DummyServiceTest.java
+++ src/test/java/com/zebrunner/DummyServiceTest.java
@@ -1,13 +1,26 @@
 package com.zebrunner;
 
+import com.zebrunner.agent.core.annotation.Maintainer;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
 
+@Maintainer("admin")
 public class DummyServiceTest {
 
-    @Test()
+    @Test(testName = "Test #1")
     public void firstTest() {
+        assertTrue(Boolean.TRUE);
+    }
+
+    @Test()
+    @Maintainer("anonymous")
+    public void secondTest() {
+        assertTrue(Boolean.TRUE);
+    }
+
+    @Test()
+    public void thirdTest() {
         assertTrue(Boolean.TRUE);
     }
 
