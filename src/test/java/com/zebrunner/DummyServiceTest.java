fc98af81733efe4289f6a7c5ce6d9e3f272d62a1
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-18 12:57:27 +0300

    012. TestNG - tests with retries

--- src/test/java/com/zebrunner/DummyServiceTest.java
+++ src/test/java/com/zebrunner/DummyServiceTest.java
@@ -1,7 +1,6 @@
 package com.zebrunner;
 
-import com.zebrunner.agent.core.annotation.TestLabel;
-import com.zebrunner.agent.core.registrar.Label;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
@@ -13,27 +12,16 @@ public class DummyServiceTest {
         assertTrue(Boolean.TRUE);
     }
 
-    @Test(testName = "Second")
-    public void secondTest() {
-        assertTrue(Boolean.TRUE);
+    @DataProvider(name = "numbers-provider")
+    public Object[][] numbers() {
+        return new Object[][] { { 1 }, { 2 }, { 3 } };
     }
 
-    @Test()
-    public void thirdTest() {
-        assertTrue(Boolean.FALSE);
-    }
-
-    @Test(dependsOnMethods = "thirdTest")
-    @TestLabel(name = "test", value = "test-val") // will be attached
-    public void fourthTest() {
-        Label.attach("label", "depends-on-3rd"); // not gonna be attached, since test body never executed
-        assertTrue(Boolean.TRUE);
-    }
-
-    @Test(dependsOnMethods = "secondTest")
-    public void fifthTest() {
-        Label.attach("label", "depends-on-2nd");
-        assertTrue(Boolean.TRUE);
+    // if test will fail - try to execute it up to 3 times
+    @Test(dataProvider = "numbers-provider", retryAnalyzer = CustomRetryAnalyzer.class)
+    public void testWithDP(int number) {
+        boolean result = number == 2 ? Boolean.FALSE : Boolean.TRUE;
+        assertTrue(result);
     }
 
 }
