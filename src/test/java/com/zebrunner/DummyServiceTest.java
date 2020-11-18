b6cb9ff179a0ee8905b2b3deeefaf78a44ddb954
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-18 12:16:43 +0300

    008. TestNG - invocation count, disabled tests

--- src/test/java/com/zebrunner/DummyServiceTest.java
+++ src/test/java/com/zebrunner/DummyServiceTest.java
@@ -1,59 +1,34 @@
 package com.zebrunner;
 
-import com.zebrunner.agent.core.registrar.Artifact;
-import com.zebrunner.agent.core.registrar.ArtifactReference;
-import com.zebrunner.agent.core.registrar.Screenshot;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
+import java.util.Random;
 
 import static org.testng.Assert.assertTrue;
 
 public class DummyServiceTest {
 
-    private static final Logger log = LoggerFactory.getLogger(DummyServiceTest.class);
+    private static final Random r = new Random();
 
-    @Test(testName = "Test #1")
-    public void firstTest() throws IOException {
-        log.info("Doing things: complex assertions, you know...");
-
-        Screenshot.upload(Files.readAllBytes(FileUtils.IMAGES[0].toPath()), System.currentTimeMillis());
-        Artifact.upload(FileUtils.DOC, "document.docx");
-
-        waitHooks();
+    @Test(testName = "First")
+    public void firstTest() {
         assertTrue(Boolean.TRUE);
     }
 
-    @Test()
-    public void secondTest() throws IOException {
-        log.info("Info log");
-        Screenshot.upload(Files.readAllBytes(FileUtils.IMAGES[1].toPath()), System.currentTimeMillis());
-        log.warn("Warn log");
-        ArtifactReference.attach("google.com", "https://www.google.com/");
-
-        waitHooks();
-
+    @Test(testName = "Second")
+    public void secondTest() {
         assertTrue(Boolean.TRUE);
     }
 
-    @Test()
+    @Test(invocationCount = 3)
     public void thirdTest() {
-        waitHooks();
-        Artifact.upload(FileUtils.DOC, "another-document.docx");
-        ArtifactReference.attach("zebrunner.com", "https://zebrunner.com/");
-        ArtifactReference.attach("youtube.com", "https://www.youtube.com/");
-        assertTrue(Boolean.FALSE);
+        boolean result = r.nextInt(10) > 5 ? Boolean.TRUE : Boolean.FALSE;
+        assertTrue(result);
     }
 
-    private void waitHooks() {
-        try {
-            Thread.sleep(1000); // workaround for shutdown hooks issue
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    @Test(enabled = false) // not going to be reported
+    public void fourthTest() {
+        assertTrue(Boolean.TRUE);
     }
 
 }
