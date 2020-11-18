daab1628d639cab5369a0f0aa6f5adb642ecfb26
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-18 12:19:12 +0300

    009. TestNG - tests with data provider

--- src/test/java/com/zebrunner/DummyServiceTest.java
+++ src/test/java/com/zebrunner/DummyServiceTest.java
@@ -1,5 +1,6 @@
 package com.zebrunner;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Random;
@@ -20,15 +21,21 @@ public class DummyServiceTest {
         assertTrue(Boolean.TRUE);
     }
 
-    @Test(invocationCount = 3)
+    @Test()
     public void thirdTest() {
         boolean result = r.nextInt(10) > 5 ? Boolean.TRUE : Boolean.FALSE;
         assertTrue(result);
     }
 
-    @Test(enabled = false) // not going to be reported
-    public void fourthTest() {
-        assertTrue(Boolean.TRUE);
+    @DataProvider(name = "numbers-provider")
+    public Object[][] numbers() {
+        return new Object[][] { { 1 }, { 2 }, { 3 } };
+    }
+
+    @Test(dataProvider = "numbers-provider")
+    public void testWithDP(int number) {
+        boolean result = number == 2 ? Boolean.FALSE : Boolean.TRUE;
+        assertTrue(result);
     }
 
 }
